1d31501e1b2e03c7f36e5f4a2423d5f5d2562f4f
Noussaier Salaani <noussaier@MacBookPro-de-Noussaier.local>
2026-02-03 10:07:41 -0500

    fix(ui): move avatar even lower (+20px)

--- src/screens/profile/UserProfileScreen.tsx
+++ src/screens/profile/UserProfileScreen.tsx
@@ -1249,7 +1249,7 @@ const createStyles = (colors: ThemeColors, isDark: boolean) => StyleSheet.create
     bottom: 0,
   },
   coverSpacer: {
-    height: COVER_HEIGHT - 40,
+    height: COVER_HEIGHT - 20,
   },
   backBtn: {
     position: 'absolute',
